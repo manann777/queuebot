3587db3495b8e7cead33c8a9bc6aaa3462065549
passji jinota <passji jinota>
2017-03-19 08:23:54 +0700

    editgulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
 
-gulp.task('default', function() {
+gulp.task('default',['browser-sync'], function() {
   // place code for your default task here
+    gulp.watch(['**/*.html'], browserSync.reload);
+    gulp.watch(['css/**/*.css'], browserSync.reload);
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
